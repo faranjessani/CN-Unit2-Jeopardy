53e9fc9a2c6befdde1211b9803a07cbd7076992f
Faran Jessani <faran@jessani.net>
2020-08-31 19:33:07 -0400

    Replaced static html with html generated in js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
-$(function(){
+$(function() {
     function resizeFunction() {
-        var textHeight = Math.max.apply(null, ($('.category-title').map(function(){return $(this).height();})));
-	    var width = Math.max.apply(null, ($('.category-title').map(function(){return $(this).parent().width();})));
-	    var aspectRatioHeight = width * (9 / 16);
-	    var height = Math.max(textHeight, aspectRatioHeight);
-	    $('.category-title').height(height).width(width);
+        var textHeight = Math.max.apply(null, ($('.category-cell').map(function() { return $(this).height(); })));
+        var width = Math.max.apply(null, ($('.category-cell').map(function() { return $(this).parent().width(); })));
+        var aspectRatioHeight = width * (9 / 16);
+        var height = Math.max(textHeight, aspectRatioHeight);
+        $('.category-cell').height(height).width(width);
     };
     $(window).resize(resizeFunction);
+    renderCategories();
+    renderQuestions();
     resizeFunction();
 });
