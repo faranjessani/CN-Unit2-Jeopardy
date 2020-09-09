0f6c943787f27e0da8cf37cf7fc6968d105be04f
Faran Jessani <faran@jessani.net>
2020-09-09 08:00:55 -0400

    Added some CSS and instructions.

--- scripts/render_questions.js
+++ scripts/render_questions.js
@@ -13,7 +13,7 @@ let questionValues = [100, 200, 300, 400, 500];
  *      Need more help? Check out https://mzl.la/3lDHxIq or ask a volunteer.
  * 
  * 3. Create an HTML string for each question. It should look like this:
- *      <div class='question'><div class='question-title'>VALUE</div></div>
+ *      <div class='question'><div class='question-cell'>VALUE</div></div>
  * 
  * 4. Replace VALUE in each string with the question's value.
  *      For example, the $500 question would be:
@@ -25,7 +25,14 @@ let questionValues = [100, 200, 300, 400, 500];
  *      HINT: Use $(selector).append(html);
  *      Need more help? Check out https://api.jquery.com/append/ or ask a volunteer.
  * 
- * 6. TODO(faranjessani): Add an instruction for the data-category attribute.
+ * 6. Update the <div> with class='question-cell' to include an attribute data-category.
+ *      The value for data-category should be the value of 'i' from your loop. 
+ *      The resulting <div> for i = 0 should look like this:
+ *          <div class='question-cell' data-category='0'>VALUE</div>
+ *      For i = 1,
+ *          <div class='question-cell' data-category='1'>VALUE</div>
+ *      HINT: Use the + operator on a string and a variable to join them!
+ *      Need more help? Check out https://bit.ly/2EIsD3b or ask a volunteer.
  */
 function renderQuestions() {
     for (let i = 0; i < numCategories; i++) {
